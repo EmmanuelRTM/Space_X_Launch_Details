10db63f26100c15872e72639d0bef545e5e30a4a
Mario Emmanuel Rodriguez Trejo <ing.emmanuel.rodriguez.t@gmail.com>
2021-02-28 10:09:04 -0500

    added launches and launches items with cross functional backend and front end

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 //simple setup
 
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema =require ('./schema');
-
 const app = express();
 
+//app.use(cors());
+
+
+/*const corsOptions = {
+    origin: 'http://localhost:3000/',
+    credentials: true,
+    };*/
+
+    app.use(cors());
+
+//allow cross-origin
 app.use( '/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true,
