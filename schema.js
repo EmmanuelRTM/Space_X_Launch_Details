8421fefb8826cbddcb228e8ddfd772407cc63d76
Mario Emmanuel Rodriguez Trejo <ing.emmanuel.rodriguez.t@gmail.com>
2021-03-18 21:15:05 -0500

    prepared code to deploy

--- schema.js
+++ schema.js
@@ -6,10 +6,11 @@ const LaunchType = new GraphQLObjectType({
     name: 'Launch',
     fields: () =>({
         flight_number: {type: GraphQLInt},
+
         mission_name:{type: GraphQLString},
         launch_year:{type: GraphQLString},
         launch_date_local:{type: GraphQLString}, 
-        launch_sucess:{type: GraphQLBoolean},
+        launch_success:{type: GraphQLBoolean},
         rocket: {type: RocketType}
     })
 });
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema =require ('./schema');
 const app = express();
+const path=require('path')
 
 //app.use(cors());
 
@@ -23,6 +24,11 @@ app.use( '/graphql', graphqlHTTP({
     }),
 );
 
+app.use(express.static('public'))
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public','index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen( PORT, () => console.log(
